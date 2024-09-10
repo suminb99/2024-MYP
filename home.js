e43ec6f6bef862b1dbe0e1d4919a1f83f28bb963
suminb99 <suminbaek99@gmail.com>
2024-09-10 21:39:11 +0900

    Design: Masonry 레이아웃 home.html에 적용

--- home.js
+++ home.js
@@ -4,6 +4,7 @@ for (let i=1; i<6; i++) {
     const imgElement = document.createElement('img');
     imgElement.src = `https://raw.githubusercontent.com/suminb99/2024-MYP/main/assets/images/img${i}.jpeg`;
     imgElement.alt = `Meme Template ${i}`;
+    
     imgElement.classList.add('meme-template')
 
     imgElement.addEventListener('click', function() {
