fbcca925fd7046a850227336c223e3af85cdc916
suminb99 <suminbaek99@gmail.com>
2024-09-12 15:07:36 +0900

    Feat: 템플릿 이미지 추가

--- home.js
+++ home.js
@@ -1,6 +1,6 @@
 const memeTemplates = document.getElementById('meme-templates');
 
-for (let i=1; i<6; i++) {
+for (let i=1; i<9; i++) {
     const imgElement = document.createElement('img');
     imgElement.src = `https://raw.githubusercontent.com/suminb99/2024-MYP/main/assets/images/img${i}.jpeg`;
     imgElement.alt = `Meme Template ${i}`;
