fefc0265515286b9af6e6cf585191c74b6df1e4e
suminb99 <suminbaek99@gmail.com>
2024-09-29 17:45:12 +0900

    Feat: images.js에 사진 정보 추가

--- images.js
+++ images.js
@@ -44,6 +44,41 @@ const memeImages = [
         width: 552,
         height: 552,
     },
+    {
+        img: '10',
+        width: 735,
+        height: 715,
+    },
+    {
+        img: '11',
+        width: 606,
+        height: 607,
+    },
+    {
+        img: '12',
+        width: 640,
+        height: 641,
+    },
+    {
+        img: '13',
+        width: 735,
+        height: 700,
+    },
+    {
+        img: '14',
+        width: 736,
+        height: 728,
+    },
+    {
+        img: '15',
+        width: 735,
+        height: 1081,
+    },
+    {
+        img: '16',
+        width: 736,
+        height: 736,
+    },
 ];
 
 export default memeImages;
